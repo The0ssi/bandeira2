b9ffe156462dc079b92396fdab8ff3fc032d8846
The0ssi <beatriz.r.ferreira@aluno.senai.br>
2025-05-29 17:21:25 -0300

    Delete Bandeira-main directory

--- Bandeira-main/js/inicio.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var botaoJogar = document.getElementById("jogarBnt");
-    if (botaoJogar) {
-        botaoJogar.addEventListener("click", function () {
-            window.location.href = "html/login.html";
-        });
-    }
-});
-
